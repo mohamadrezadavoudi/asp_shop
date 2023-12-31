103284e60bbd5a237a9ac36908d0a9dfed9f8e49
mohamadreza <mohamadrezadavoudi.11@gmail.com>
2023-12-31 19:16:34 +0330

    second commit

---
+++ asp_shop_prj/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace asp_shop_prj.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
